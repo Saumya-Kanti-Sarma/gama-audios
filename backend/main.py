7842f49d64c0ce06e6722a1fc0159937b18be8f3
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2025-06-25 23:36:30 +0530

    fast api ready

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,30 @@
 from fastapi import FastAPI
-from pytubefix import YouTube
-from pytubefix.cli import on_progress
+from utils.downloadMP3 import DownloadAudio
+from dotenv import load_dotenv
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+import shutil
+import os
+
+load_dotenv()
+URL = os.environ.get("URL")
 app = FastAPI()
 
+origins = [
+    "http://localhost.8000",
+    URL
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.mount("/downloads", StaticFiles(directory="downloads"), name="downloads")
 
 @app.get("/")
 async def root():
@@ -11,3 +33,8 @@ async def root():
 @app.get("/items/{item_id}")
 async def read_item(item_id):
     return {"item_id": item_id}
+
+@app.post("/download")
+async def read_item():
+    shutil.rmtree("downloads")
+    return {"url": f"{URL}{DownloadAudio()}"}
