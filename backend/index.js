a4f3f01766a951e0a35f60272c6b0081a067c023
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2025-07-28 13:07:01 +0530

    server ready

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 import express from "express";
 import { configDotenv } from "dotenv";
+import { mp3Downloader } from "./utils/downloader.util.js";
 
 configDotenv();
+
 const app = express();
 const URI = process.env.URI;
+const download = new mp3Downloader();
 
 app.use(express.json());
 
@@ -12,6 +15,25 @@ app.get("/", (req, res) => {
     status: 201,
     message: "this is gama audios"
   })
+})
+
+app.get("/download-mp3/:url", async (req, res) => {
+  try {
+    const url = req.params["url"];
+    const newUrl = "https://youtu.be/" + url;
+    const link = await download.downloadMp3(newUrl);
+    res.send({
+      status: 202,
+      provided_url: newUrl,
+      data: link
+    })
+  } catch (error) {
+    res.send({
+      status: 501,
+      message: "error in calling download function",
+      error: error.message
+    })
+  }
 })
 
 app.listen(3000, () => {
