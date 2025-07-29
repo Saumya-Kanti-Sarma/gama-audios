240275a81abebb8acb2c6f812f17204abeeab892
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2025-07-29 23:16:50 +0530

    added cors and minor tweeks

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { configDotenv } from "dotenv";
+import cors from "cors";
 import { mp3Downloader } from "./utils/downloader.util.js";
 
 configDotenv();
@@ -9,6 +10,8 @@ const URI = process.env.URI;
 const download = new mp3Downloader();
 
 app.use(express.json());
+app.use(cors());
+
 
 app.get("/", (req, res) => {
   res.send({
@@ -17,16 +20,35 @@ app.get("/", (req, res) => {
   })
 })
 
-app.get("/download-mp3/:url", async (req, res) => {
+app.post("/download-mp3/", async (req, res, next) => {
+  const key = req.query["key"];
+  const KEY = process.env.MY_API_KEY
+  if (!key || key !== KEY) {
+    res.send({
+      status: 401,
+      message: "Invalid API key"
+    })
+  }
+  else {
+    next()
+  }
+}, async (req, res) => {
   try {
-    const url = req.params["url"];
-    const newUrl = "https://youtu.be/" + url;
-    const link = await download.downloadMp3(newUrl);
+    const data = req.body;
+    if (!data) {
+      res.send({
+        status: 300,
+        message: "please provide a valid url to download",
+      })
+    }
+    const link = await download.downloadMp3(data.url);
     res.send({
       status: 202,
-      provided_url: newUrl,
+      provided_url: data.url,
       data: link
     })
+    console.log("video converted");
+
   } catch (error) {
     res.send({
       status: 501,
@@ -35,6 +57,7 @@ app.get("/download-mp3/:url", async (req, res) => {
     })
   }
 })
+
 
 app.listen(3000, () => {
   console.log(`server running in ${URI}`);
